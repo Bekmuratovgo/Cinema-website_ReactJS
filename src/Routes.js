b943423c9c11d7eb7fde6d81a657aa3401b94384
  Akniet <akniet@MacBook-Pro-Akniet.local>
2021-04-29 16:05:57 +0600

    signup-sign-in

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Switch,  Route} from 'react-router-dom';
-import Home from './components/Home';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import AuthContextProvider from "./context/AuthContext";
 
 const Routes = () => {
     return (
-        <>
-        <BrowserRouter>
-            <Navbar />
-            <Switch>
-                <Route exact path="/" component={Home}  />
-                {/* <Route exact path="/" component={} />
+        <AuthContextProvider>
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/signup" component={SignUp} />
+                    <Route exact path="/login" component={SignIn} />
+
+                    {/* <Route exact path="/" component={} />
                 <Route exact path="/" component={}/>
                 <Route exact path="/" component={} /> */}
-
-            </Switch>
-            
-        </BrowserRouter>
-        </>
+                </Switch>
+            </BrowserRouter>
+        </AuthContextProvider>
     );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-  const [click, setClick] = useState(false);
+    const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
-  const closeMenu = () => setClick(false);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
 
-
-  return (
-    <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
-            M
-            <i class="fas fa-compact-disc"></i>
-            VIE
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMenu}>Home</Link>
-            </li>
-            <li className='nav-item'><Link to='/movie' className='nav-links' onClick={closeMenu}>Movie</Link>
-            </li>
-            <li className='nav-item'><Link to='/coming' className='nav-links' onClick={closeMenu}>Coming soon</Link>
-            </li>
-            <li className='search-item'><input placeholder="Search" /></li>
-            <li className='nav-item'><Link to='/' className='nav-links' onClick={closeMenu}><i class="fas fa-shopping-cart"></i></Link>
-            </li>
-            <li>
-              <Link 
-                to='/sign-up'
-                className='nav-links'
-                onClick={closeMenu}
-              > 
-                Sign Up
-                
-              </Link>
-            </li>   
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        M<i class="fas fa-compact-disc"></i>
+                        VIE
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link
+                                to="/"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/coming"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
+                                Coming soon
+                            </Link>
+                        </li>
+                        <li className="search-item">
+                            <input placeholder="Search" />
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
+                                <i class="fas fa-shopping-cart"></i>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/signup"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
+                                Sign Up
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/login"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
+                                Sign In
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
